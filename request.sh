0744cded4b26755eaa8f1c264f42985bde0fd116
Pavel Ogorodnikov <asapawell@gmail.com>
2021-04-28 20:21:58 +0300

    добавлен файл с запросом

--- request.sh
+++ request.sh
@@ -1,3 +1,3 @@
 !#bin/bash
-echo "отправка GET запроса"
-GET https://api.quarantine.country/api/v1/regions/HTTP/1.1
+echo "запрос на сервер с помощью curl"
+curl https://api.quarantine.country/api/v1/regions
